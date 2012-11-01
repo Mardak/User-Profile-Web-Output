418b28944f4d8edf976ecfe424e5d5a51fadbd77
Aaron Druck <aldruck@gmail.com>
2012-10-31 18:02:03 -0700

    Made some changes to the JS and some interactions

--- script/effects.js
+++ script/effects.js
@@ -1,6 +1,17 @@
 var shareLevel = 1;
 
 
+// my custom tooltip
+function tooltip() {
+	// create tooltip
+	$('<p id="test">My <em>new</em> text</p>').appendTo('body').animate({opacity: "0.0"}, 1020);
+	
+	//animate tooltip
+	
+	//hide tooltip
+}
+
+
 $(document).ready(function(){
 	
 	var arrayData = [
@@ -74,20 +85,27 @@ $(document).ready(function(){
     });
 	
 	$(".image400x140").hover(function() {
-	    $("ul#interest1 li.level1").css("opacity", "0.5");
+		tooltip();
+		$("ul#interest1 li.level1").stop().animate({opacity: "0.8"}, 120);
 	}, function() {
-	    $("ul#interest1 li.level1").css("opacity", "1");
+		$("ul#interest1 li.level1").stop().animate({opacity: "1"}, 120);
 	});
 	
 	$("ul#interest1 li.level1").hover(function(e) {
-		$("ul#interest1 li.level1").css("opacity", "0.5");
+		$("ul#interest1 li.level1").css("cursor", "pointer");
+		$("ul#interest1 li.level1").stop().animate({opacity: "0.8"}, 120);
         $(".image400x140").children('a').children('img').animate({ height: '299', left: '0', top: '0', width: '450'}, 100);
         $(".image400x140").children('a').children('span').fadeIn(200);
     }).mouseleave(function(e) {
         $(".image400x140").children('a').children('img').animate({ height: '332', left: '-20', top: '-20', width: '500'}, 100);
         $(".image400x140").children('a').children('span').fadeOut(200);
-		$("ul#interest1 li.level1").css("opacity", "1");
+		$("ul#interest1 li.level1").stop().animate({opacity: "1"}, 120);
     });
+
+
+
+
+	
 	
 
 	
@@ -99,30 +117,53 @@ $(document).ready(function(){
 		            range: "min",
 		            min: 0,
 		            max: 100,
-		            value: 20,
-		            slide: function( event, ui ) {
-		                $( "#amount" ).val( ui.value );
-		            }
-		        }
-			);
-	        // $( "#amount" ).val( $( "#slider-horizontal" ).slider( "value" ) );
+		            value: 37,
+		            slide: function(event, ui) {
+							if (ui.value == 0) {
+								// turn off all levels
+								$("li.level1").stop().animate({opacity: "0.3"}, 120);
+								
+							}
+							
+							if (ui.value == 37) {
+								// turn off all levels
+								$("li.level1").stop().animate({opacity: "1"}, 120);
+								
+							}
+							
+					},
+					stop: function(event, ui) {     
+					}
+				});
 	    });
 	
 	
-	
-    $(function() {
+
+    
+	$(function() {
         $( "#slider" ).slider({
 			orientation: "horizontal",
 			range: "min",
             value: 0,
             min: 0,
             max: 100,
-            slide: function( event, ui ) {
-                $( "#amount" ).val( ui.value );
-            }
+            slide: function(event, ui) {
+			                                $("#slider_value").val(ui.value);
+			                          },
+			            stop: function(event, ui) {
+			            alert(ui.value);
+			                $sliderValue=ui.value;
+			            }
         });
-        $( "#amount" ).val( $( "#slider" ).slider( "value" ) );
+
+		
+        // $( "#amount" ).val( $( "#slider" ).slider( "value" ) );
+
+		// alert(value);
+
     });
+
+
 
 	$(function() {
         $( "#slider2" ).slider({
